98b9f3e78b7a35f9ca6b9aae4babf0ce42b73ef5
Wissam Tahiri <elhou@MacBook-Pro.local>
2026-06-05 16:54:36 +0200

    ajout export postman et bdd

--- resources/vikings.sql
+++ resources/vikings.sql
@@ -1,40 +1,35 @@
--- phpMyAdmin SQL Dump
--- Base de données : `vikings`
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- MySQL dump 10.13  Distrib 8.0.44, for macos12.7 (arm64)
+--
+-- Host: localhost    Database: vikings
+-- ------------------------------------------------------
+-- Server version	8.0.44
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
--- --------------------------------------------------------
--- Structure de la table `weapon`
--- --------------------------------------------------------
-
-CREATE TABLE `weapon` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` varchar(50) NOT NULL,
-  `damage` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Données de la table `weapon`
+-- Current Database: `vikings`
 --
 
-INSERT INTO `weapon` (`type`, `damage`) VALUES
-('sword', 150),
-('axe', 200),
-('bow', 80),
-('spear', 120);
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `vikings` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
--- --------------------------------------------------------
--- Structure de la table `viking`
--- --------------------------------------------------------
+USE `vikings`;
 
+--
+-- Table structure for table `viking`
+--
+
+DROP TABLE IF EXISTS `viking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `viking` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(16) NOT NULL,
@@ -43,21 +38,57 @@ CREATE TABLE `viking` (
   `health` int NOT NULL,
   `weaponId` int DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `fk_viking_weapon` (`weaponId`),
   CONSTRAINT `fk_viking_weapon` FOREIGN KEY (`weaponId`) REFERENCES `weapon` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Données de la table `viking`
+-- Dumping data for table `viking`
 --
 
-INSERT INTO `viking` (`name`, `attack`, `defense`, `health`, `weaponId`) VALUES
-('Ragnar', 200, 150, 300, 1),
-('Floki', 150, 80, 350, NULL),
-('Lagertha', 300, 200, 200, 2),
-('Björn', 350, 200, 100, NULL);
+LOCK TABLES `viking` WRITE;
+/*!40000 ALTER TABLE `viking` DISABLE KEYS */;
+INSERT INTO `viking` VALUES (1,'Ragnar',200,150,300,1),(2,'Floki',150,80,350,NULL),(3,'Lagertha',300,200,200,2),(4,'Björn',350,200,100,NULL),(5,'Ivar',300,100,250,1);
+/*!40000 ALTER TABLE `viking` ENABLE KEYS */;
+UNLOCK TABLES;
 
-COMMIT;
+--
+-- Table structure for table `weapon`
+--
 
+DROP TABLE IF EXISTS `weapon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `weapon` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL,
+  `damage` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weapon`
+--
+
+LOCK TABLES `weapon` WRITE;
+/*!40000 ALTER TABLE `weapon` DISABLE KEYS */;
+INSERT INTO `weapon` VALUES (1,'sword',150),(2,'axe',200),(3,'bow',80),(4,'spear',120);
+/*!40000 ALTER TABLE `weapon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'vikings'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-05 16:53:59
